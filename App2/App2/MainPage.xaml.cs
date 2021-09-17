6807e6446394e2d963a727409f620d28ac66072b
Adriana <craftczc@gmail.com>
2021-09-17 17:58:48 -0500

    Agregar archivos de proyecto.

---
+++ App2/App2/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using static AndroidX.Core.App.NotificationCompat.MessagingStyle;
+
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+        int cont;
+       string message;
+        Button button1;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            cont = 0;
+        }
+
+
+            void Handle_Clicked(object sender, System.EventArgs e)
+            {
+                if (button1 == null)
+                
+                    button1=((Button)sender);
+                
+                cont++;
+                message = "You click me " + cont + " times";
+                button1.Text= string.Format(message,cont);
+            }
+
+            void Handle_Clicked_1(object sender, System.EventArgs e)
+            {
+                message = "Click me";
+                if (button1 != null)
+                {
+                    button1.Text = message;
+                cont = 0;
+                }
+            }
+
+        }
+    }
+
